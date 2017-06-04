402cab910594964952182e2707fe5383f3238d07
Rémy Woine <remy.woine@gmail.com>
2017-06-04 16:39:56 +0200

    Need to fix inter-thread exchange.

--- WLinkToolBox/Form1.Designer.cs
+++ WLinkToolBox/Form1.Designer.cs
@@ -29,11 +29,184 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBoxDebug = new System.Windows.Forms.TextBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.comboBoxPortComList = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timerRefreshCom = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1008, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripSeparator1,
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Location = new System.Drawing.Point(0, 28);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1008, 600);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1000, 574);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Parameters";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1000, 574);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "WCommand";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBoxDebug
+            // 
+            this.textBoxDebug.Location = new System.Drawing.Point(4, 634);
+            this.textBoxDebug.Multiline = true;
+            this.textBoxDebug.Name = "textBoxDebug";
+            this.textBoxDebug.ReadOnly = true;
+            this.textBoxDebug.Size = new System.Drawing.Size(1000, 88);
+            this.textBoxDebug.TabIndex = 2;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(1000, 574);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "WConfig";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(1000, 574);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "EEPROM";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // comboBoxPortComList
+            // 
+            this.comboBoxPortComList.FormattingEnabled = true;
+            this.comboBoxPortComList.Location = new System.Drawing.Point(837, 728);
+            this.comboBoxPortComList.Name = "comboBoxPortComList";
+            this.comboBoxPortComList.Size = new System.Drawing.Size(91, 21);
+            this.comboBoxPortComList.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(934, 728);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(70, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timerRefreshCom
+            // 
+            this.timerRefreshCom.Interval = 1000;
+            this.timerRefreshCom.Tick += new System.EventHandler(this.timerRefreshCom_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1008, 761);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBoxPortComList);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.textBoxDebug);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "W-Link Toolbox";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox comboBoxPortComList;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timerRefreshCom;
+        public System.Windows.Forms.TextBox textBoxDebug;
     }
 }
 
--- WLinkToolBox/Form1.cs
+++ WLinkToolBox/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,91 @@ using System.Windows.Forms;
 
 namespace WLinkToolBox
 {
+
     public partial class Form1 : Form
     {
+
+        String[] GL_pComPort_Str = SerialPort.GetPortNames();
+
         public Form1()
         {
             InitializeComponent();
+
+            // Setup Debug interface
+            comboBoxPortComList.Items.Clear();
+            comboBoxPortComList.Items.AddRange(GL_pComPort_Str);
+            if (GL_pComPort_Str.Length != 0)
+            {
+                comboBoxPortComList.SelectedIndex = 0;
+                serialPort1.PortName = GL_pComPort_Str[comboBoxPortComList.SelectedIndex];
+            }
+            else
+            {
+                comboBoxPortComList.Text = "";
+            }
+
+            timerRefreshCom.Start();
+
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(button1.Text == "Open")
+            {
+                if (GL_pComPort_Str.Length != 0)
+                {
+                    button1.Text = "Close";
+                    serialPort1.PortName = GL_pComPort_Str[comboBoxPortComList.SelectedIndex];
+                    comboBoxPortComList.Enabled = false;
+
+                    serialPort1.Open();
+                    timerRefreshCom.Stop();
+                }
+
+            } else
+            {
+                button1.Text = "Open";
+                comboBoxPortComList.Enabled = true;
+
+                serialPort1.Close();
+                timerRefreshCom.Start();
+            }
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            Console.Write(sp.ReadExisting());
+            //textBoxDebug.AppendText(sp.ReadExisting());
+        }
+
+        private void timerRefreshCom_Tick(object sender, EventArgs e)
+        {
+            if(!serialPort1.IsOpen)
+            {
+                if (GL_pComPort_Str.Length != SerialPort.GetPortNames().Length)
+                {
+                    // Refresh Port COM
+                    GL_pComPort_Str = SerialPort.GetPortNames();
+
+                    // Setup Debug interface
+                    comboBoxPortComList.Items.Clear();
+                    comboBoxPortComList.Items.AddRange(GL_pComPort_Str);
+                    if (GL_pComPort_Str.Length != 0)
+                    {
+                        comboBoxPortComList.SelectedIndex = 0;
+                        serialPort1.PortName = GL_pComPort_Str[comboBoxPortComList.SelectedIndex];
+                    } else
+                    {
+                        comboBoxPortComList.Text = "";
+                    }
+                }
+            }
         }
     }
 }
